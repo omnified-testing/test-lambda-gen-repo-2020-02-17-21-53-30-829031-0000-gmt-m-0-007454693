2fd8fda67b0954a720c69f9d87950eb44a9beab4
Adam Harris <adam@omnified.io>
2020-02-17 21:53:34 +0000

    Auto Generated boilerplate file.

---
+++ utility/utility.go
@@ -0,0 +1,11 @@
+package utility
+
+// import (
+// 	// Imports can eventually be automatically inserted here
+// )
+
+// Call : Describe what the function does here.
+// Put the function's code in the Call function below.
+func Call(s string ) s string{
+	return s + " there."
+}
